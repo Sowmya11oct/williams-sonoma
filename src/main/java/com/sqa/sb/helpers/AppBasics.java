da5e033120619a865147359ba5cefce281b32765
Sowmya Balachandran <sowmya.11oct87@gmail.com>
2017-05-03 20:59:52 -0700

    Update AppBasics with More Request Methods

--- src/main/java/com/sqa/sb/helpers/AppBasics.java
+++ src/main/java/com/sqa/sb/helpers/AppBasics.java
@@ -18,6 +18,27 @@ public class AppBasics {
 		return scanner.nextLine();
 	}
 
+	public static boolean requestBoolean(String question) {
+		System.out.println(question + "true/false");
+		String input = scanner.nextLine();
+		boolean value = Boolean.parseBoolean(input);
+		return value;
+	}
+
+	public static char requestChar(String question) {
+		System.out.println(question);
+		String input = scanner.nextLine();
+		char character = input.charAt(0);
+		return character;
+	}
+
+	public static float requestFloat(String question) {
+		System.out.println(question);
+		String input = scanner.nextLine();
+		float value = Float.parseFloat(input);
+		return value;
+	}
+
 	/**
 	 * @param string
 	 * @return
@@ -28,5 +49,26 @@ public class AppBasics {
 		String input = scanner.nextLine();
 		int value = Integer.parseInt(input);
 		return value;
+	}
+
+	public static long requestLong(String question) {
+		System.out.println(question);
+		String input = scanner.nextLine();
+		long value = Long.parseLong(input);
+		return value;
+	}
+
+	public static short requestShort(String question) {
+		System.out.println(question);
+		String input = scanner.nextLine();
+		short value = Short.parseShort(input);
+		return value;
+	}
+
+	public static String requestString(String question) {
+		System.out.println(question);
+		String input = scanner.nextLine();
+		// int value = Integer.parseInt(input);
+		return input;
 	}
 }
